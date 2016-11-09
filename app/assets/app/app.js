ab62adb0a66c7351d083cd97ce1c4173793adb29
ntalhi <ntalhi@LBNL031447.corp.capgemini.com>
2016-11-09 13:08:49 +0100

    Added customer view of available information. Added customer add product to cart

--- app/assets/app/app.js
+++ app/assets/app/app.js
@@ -1,11 +1,22 @@
 angular.module('myApp', [
     'ngRoute',
     'ngCookies',
+    //'ngFilter',
     'myApp.services',
     'myApp.controllers'
 ]).
     config(function ($routeProvider) {
         $routeProvider.when('/home', {templateUrl: 'views/ProductListItem.html', controller: 'ProductsController'});
+        $routeProvider.when('/shoppingCart', {templateUrl: 'views/ShoppingCartListItem.html', controller: 'ShoppingCartController'});
         
         $routeProvider.otherwise({redirectTo: '/home'});
     });
+/*
+    .filter('groupBy', function($parse) {
+        return _.memoize(function(items, field) {
+            var getter = $parse(field);
+            return _.groupBy(items, function(item) {
+                return getter(item);
+            });
+        });
+    });*/
--- app/assets/app/controllers.js
+++ app/assets/app/controllers.js
@@ -4,7 +4,7 @@ angular.module('myApp.controllers', []).
         controller('ProductsController', function ($scope, Products, $cookieStore) {
             $scope.allProducts = Products.findAll();
 
-            $scope.addToCart = function (p) {
+            $scope.addToCart = function (product) {
                 console.log("ProductsController - addToCart");
                 
                 //Get carts in shoppingCart
@@ -12,21 +12,53 @@ angular.module('myApp.controllers', []).
                 
                 //Add the item to the shoppingcart
                 if(shoppingCart == null){
-                    $cookieStore.put("shoppingCart", [p]);
+                    shoppingCart = $cookieStore.put("shoppingCart", [{product, 'quantity':1}]);
                 } else {
-                    shoppingCart.push(p);
-                    $cookieStore.put("shoppingCart", shoppingCart);
+                    
+                    var productFound = false;
+                    var tempCart = new Array();
+                    angular.forEach(shoppingCart, function(row){
+                        if(row.product.id == product.id){
+                            row.quantity = row.quantity +1;
+                            
+                            productFound = true;
+                            
+                        }
+                        
+                        tempCart.push(row);
+                    });
+                    
+                    if(!productFound){
+                        tempCart.push({product, 'quantity':1})
+                       shoppingCart = $cookieStore.put("shoppingCart", tempCart);
+                    } else {
+                        shoppingCart = $cookieStore.put("shoppingCart", tempCart);
+                    }
+                    
                 }
-                
-                console.log($cookieStore.get("shoppingCart"));
-                
+                                
             };
         }).
         controller('ShoppingCartController', function ($scope, $cookieStore) {
+            $scope.CartItems = $cookieStore.get("shoppingCart");
             $scope.CountCartItems = function () {
                 
-                return shoppingCart = $cookieStore.get("shoppingCart").count;
+                $scope.value = 1;//$cookieStore.get("shoppingCart").length;
                 
                 
             };
+            
+            $scope.getTotal = function(){
+                var total = 0;
+                var shoppingCart = $cookieStore.get("shoppingCart");
+                angular.forEach(shoppingCart, function(row){
+                        if(row.product.id == product.id){
+                            row.quantity = row.quantity +1;
+                            productFound = true;
+                        }
+                        
+                        tempCart.push(row);
+                    });
+                return total;
+            }
         });
